fc193f7b292c465954e03ee3ed05cecbf98e5505
Owen Smith <owen@owensmith.uk>
2024-11-25 14:08:05 +0000

    build: Add gdx-freetype dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     val gdxVersion: String by project
     api("com.badlogicgames.gdx:gdx:$gdxVersion")
+    api("com.badlogicgames.gdx:gdx-freetype:$gdxVersion")
 }
--- lwjgl3/build.gradle.kts
+++ lwjgl3/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     val gdxVersion: String by project
     implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:$gdxVersion")
     implementation("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
+    implementation("com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-desktop")
     implementation(project(":core"))
 }
 
