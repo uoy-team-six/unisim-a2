53adcccaf41d0c762d5805beed0b38e8f05bf81e
JRuan05 <JiaxinRuan@outlook.com>
2024-11-25 15:21:23 +0000

    Update build.gradle.kts

--- headless/build.gradle.kts
+++ headless/build.gradle.kts
@@ -13,7 +13,17 @@ tasks.test {
     useJUnitPlatform()
 }
 
+
 plugins {
-   id 'java-library'
-   id 'jacoco'
+    jacoco
 }
+
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+
