a99ffd7684be09e0d02a3f30ede4e8cc070e253e
Owen Smith <owen@owensmith.uk>
2025-01-05 16:44:25 +0000

    screen: Add a quit button to the start menu

--- core/src/main/java/io/github/unisim/screen/StartMenuScreen.java
+++ core/src/main/java/io/github/unisim/screen/StartMenuScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -22,10 +21,6 @@ import io.github.unisim.UniSimGame;
 public class StartMenuScreen extends ScreenAdapter {
     private final UniSimGame game;
     private final Stage stage;
-    private final Table table;
-    private final Skin skin;
-    private final TextButton playButton;
-    private final TextButton settingsButton;
     private final InputMultiplexer inputMultiplexer = new InputMultiplexer();
 
     /**
@@ -34,11 +29,8 @@ public class StartMenuScreen extends ScreenAdapter {
     public StartMenuScreen(UniSimGame game) {
         this.game = game;
         stage = new Stage();
-        table = new Table();
-        skin = GlobalState.defaultSkin;
 
-        // Play button
-        playButton = new TextButton("Play", skin);
+        final var playButton = new TextButton("Play", GlobalState.defaultSkin);
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -47,8 +39,7 @@ public class StartMenuScreen extends ScreenAdapter {
             }
         });
 
-        // Leaderboard button.
-        final var leaderboardButton = new TextButton("Leaderboard", skin);
+        final var leaderboardButton = new TextButton("Leaderboard", GlobalState.defaultSkin);
         leaderboardButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -57,8 +48,7 @@ public class StartMenuScreen extends ScreenAdapter {
             }
         });
 
-        // Settings button
-        settingsButton = new TextButton("Settings", skin);
+        final var settingsButton = new TextButton("Settings", GlobalState.defaultSkin);
         settingsButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -67,13 +57,22 @@ public class StartMenuScreen extends ScreenAdapter {
             }
         });
 
-        // Add UI elements to the stage
+        final var quitButton = new TextButton("Quit", GlobalState.defaultSkin);
+        quitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        // Add UI elements to the stage in a table.
+        final var table = new Table();
         table.setFillParent(true);
-        table.center().center();
-        table.pad(100, 100, 100, 100);
+        table.pad(100);
         table.add(playButton).center().width(250).height(100).padBottom(10).row();
-        table.add(leaderboardButton).center().width(250).height(100).padBottom(10).row();
-        table.add(settingsButton).center().width(250).height(67);
+        table.add(leaderboardButton).center().width(250).height(67).padBottom(10).row();
+        table.add(settingsButton).center().width(250).height(67).padBottom(10).row();
+        table.add(quitButton).center().width(250).height(67);
         stage.addActor(table);
 
         inputMultiplexer.addProcessor(GlobalState.fullscreenInputProcessor);
@@ -106,6 +105,5 @@ public class StartMenuScreen extends ScreenAdapter {
     @Override
     public void dispose() {
         stage.dispose();
-        skin.dispose();
     }
 }
