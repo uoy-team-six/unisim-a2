59708b511c1c8c8d38df711337c923f33d38ab8d
Owen Smith <owen@owensmith.uk>
2025-01-07 13:07:18 +0000

    leaderboard: Only overwrite score if higher and add Javadoc to methods

--- core/src/main/java/io/github/unisim/Leaderboard.java
+++ core/src/main/java/io/github/unisim/Leaderboard.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * A class which represents the game leaderboard.
+ */
 public class Leaderboard {
     private final Map<String, Integer> scoreMap;
 
@@ -15,14 +18,39 @@ public class Leaderboard {
         }
     }
 
+    /**
+     * Adds a new entry to the leaderboard for the given player's name and score. If the player already exists, only
+     * overwrites their score if the given score is higher.
+     *
+     * @param name  the name of the player
+     * @param score the newest score of the player
+     */
     public void addScore(String name, int score) {
-        scoreMap.put(name, score);
+        scoreMap.merge(name, score, (oldScore, newScore) -> newScore > oldScore ? newScore : oldScore);
     }
 
+    /**
+     * Removes all leaderboard entries.
+     */
+    public void clear() {
+        scoreMap.clear();
+    }
+
+    /**
+     * Returns the score for the given player name.
+     *
+     * @param name a player name
+     * @return score if the given player name exists; 0 otherwise
+     */
     public int getScore(String name) {
         return scoreMap.getOrDefault(name, 0);
     }
 
+    /**
+     * Returns a list of player names in score-descending order.
+     *
+     * @return an immutable list of player names
+     */
     public List<String> getSortedNames() {
         return scoreMap.entrySet()
             .stream()
